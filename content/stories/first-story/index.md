8bf20440be8379f41935855bffef7fe1b63b31fb
hoang.vx <hoang.vx@glv.co.jp>
2019-06-14 16:42:44 +0900

    add new stories

--- content/stories/first-story/index.md
+++ content/stories/first-story/index.md
@@ -2,6 +2,7 @@
 title: New Beginnings
 date: "2015-05-28T22:40:32.169Z"
 description: This is a custom description for SEO and Open Graph purposes, rather than the default generated excerpt. Simply add a description field to the frontmatter.
+thumbnail: ../../assets/g12.jpg
 ---
 
 Far far away, behind the word mountains, far from the countries Vokalia and
@@ -106,3 +107,21 @@ to its own, safe country. But nothing the copy said could convince her and so it
 didn’t take long until a few insidious Copy Writers ambushed her, made her drunk
 with Longe and Parole and dragged her into their agency, where they abused her
 for their projects again and again.
+
+
+This is my first post on my new fake blog! How exciting!
+
+I'm sure I'll write a lot more interesting things in the future.
+
+Oh, and here's a great quote from this Wikipedia on
+[salted duck eggs](http://en.wikipedia.org/wiki/Salted_duck_egg).
+
+> A salted duck egg is a Chinese preserved food product made by soaking duck
+> eggs in brine, or packing each egg in damp, salted charcoal. In Asian
+> supermarkets, these eggs are sometimes sold covered in a thick layer of salted
+> charcoal paste. The eggs may also be sold with the salted paste removed,
+> wrapped in plastic, and vacuum packed. From the salt curing process, the
+> salted duck eggs have a briny aroma, a gelatin-like egg white and a
+> firm-textured, round yolk that is bright orange-red in color.
+
+![Chinese Salty Egg](./salty_egg.jpg)
--- src/pages/stories.js
+++ src/pages/stories.js
@@ -10,6 +10,7 @@ import BreadCrumb from "../components/breadcrumb";
 class Stories extends React.Component {
   render() {
     const { data } = this.props
+    const posts = data.allMarkdownRemark.edges
 
     return (
       <Layout>
@@ -19,10 +20,38 @@ class Stories extends React.Component {
           <div className="hero-body ht-card">
             <div className="container is-widescreen">
               <div className="tile ht-card-title">
-                <h1>Những Câu Chuyện</h1>
+                <h1>Ký Sự</h1>
               </div>
               <div className="tile ht-card-title">
                 <Img fixed={data.headding.childImageSharp.fixed} />
+              </div>
+              <div className="tile is-vertical">
+                {posts.map(({ node }, idx) => {
+                  const title = node.frontmatter.title || node.fields.slug
+                  if (idx % 2 == 0) { 
+                    return (
+                        <div key={node.fields.slug} className="tile">
+                          <div className="tile">
+                            <Img fixed={node.frontmatter.thumbnail.childImageSharp.fixed} />
+                          </div>
+                          <div className="tile">
+                            
+                          </div>
+                        </div>
+                      )
+                   } else {
+                    return (
+                      <div key={node.fields.slug} className="tile">
+                        <div className="tile">
+                          
+                        </div>
+                        <div className="tile">
+                          <Img fixed={node.frontmatter.thumbnail.childImageSharp.fixed} />
+                        </div>
+                      </div>
+                    )
+                   }
+                })}
               </div>
             </div>
           </div>
@@ -45,6 +74,30 @@ export const pageQuery = graphql`
       childImageSharp {
         fixed(width: 177, height: 22) {
           ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            thumbnail {
+              id
+              publicURL
+              childImageSharp {
+                fixed(height: 200, width: 300) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
         }
       }
     }
